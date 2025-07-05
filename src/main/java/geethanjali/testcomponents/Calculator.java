596649fbb594b1b38a1480f03ec8e4b1553e1298
keerthysith <keerthysith@gmail.com>
2025-07-05 21:07:27 +0530

    Calculated Added

---
+++ src/main/java/geethanjali/testcomponents/Calculator.java
@@ -0,0 +1,12 @@
+package geethanjali.testcomponents;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Calculator pgm");
+
+	}
+
+}
